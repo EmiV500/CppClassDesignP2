3b17bac452798af1c79977d14ecec07ddcb1a1c8
EmiV500 <eeveemc500@gmail.com>
2021-12-17 23:56:24 -0800

    last minute changes

--- Store.cpp
+++ Store.cpp
@@ -1,4 +1,5 @@
 #include "Store.h"
+#include "Order.h"
 #include <iostream>
 
 Store::Store(Item items[100]) {
@@ -6,9 +7,17 @@ Store::Store(Item items[100]) {
             _items[i] = items[i];
         }
     }
-void Store::printInventory() {
+void Store::printInventory() const {
     std::cout << "Store: \n";
     for (int i = 0; i < 100; i++) {
         std::cout << _items[i].getName() << " x " << _items[i].getStock() << "\n";
+    }
+}
+
+void Store::printStoreItem(const Item& item) {
+    for (int i = 0; i < 100; i++) {
+       if(const _items[i] == item) {
+           std::cout << item.getName() << item.getId() << item.getPrice() << item.getStock();
+       }
     }
 }
